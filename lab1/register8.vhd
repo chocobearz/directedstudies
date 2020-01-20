775a8b9b4810b106634c191c2361dda75e622fd6
Paige T <ptuttosi@sfu.ca>
2020-01-19 17:40:05 -0800

    fix errors in register

--- lab1/register8.vhd
+++ lab1/register8.vhd
@@ -1,26 +1,27 @@
 LIBRARY ieee ;
 USE ieee.std_logic_1164.ALL;
-USE ieee.std_logic_arith.ALL;
-USE ieee.std_logic_unsigned.ALL;
 
 ENTITY register8 IS
   PORT (
     D:   IN  STD_LOGIC_VECTOR(7 downto 0);
     Q:   OUT STD_LOGIC_VECTOR(7 downto 0);
-    ld:  IN  STD_LOGIC;
-    clr: IN  STD_LOGIC;
-    clk: IN  STD_LOGIC
+    ld:  IN  STD_LOGIC := '0';
+    clr: IN  STD_LOGIC := '0';
+    clk: IN  STD_LOGIC := '0'
   );
-END register8 ;
+END register8;
 
 ARCHITECTURE LogicFunction OF register8 IS
 BEGIN
-  IF (clr = ‘1’) THEN
-    Q <= “00000000”;
-  PROCESS (clk)
-    IF rising_edge(clk) THEN
-      IF (ld = ‘1’) THEN
-      Q <= D;
+  PROCESS( clr, clk, ld )
+  BEGIN
+    IF ( clr = '1' ) THEN
+      Q <= "00000000";
+	 ELSIF rising_edge(clk) THEN
+	 --do I need a second process here?
+      IF ( ld = '1' ) THEN
+        Q <= D;
+		END IF;
     END IF;
   END PROCESS;
 END LogicFunction;
