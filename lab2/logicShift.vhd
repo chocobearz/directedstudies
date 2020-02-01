7a8ed3b3cd41a0cebcd4923ebe61a3b0148e44fe
Paige T <ptuttosi@sfu.ca>
2020-01-31 23:23:47 -0800

    Fix OP error in logic shift

--- lab2/logicShift.vhd
+++ lab2/logicShift.vhd
@@ -5,20 +5,23 @@ USE ieee.numeric_std.all;
 ENTITY logicShift IS
   GENERIC(n: INTEGER:= 3);
   PORT(
-    OP:  IN  STD_LOGIC_VECTOR(2 downto 0);
+    OP:  IN  STD_LOGIC_VECTOR (1 downto 0);
     A,B: IN  STD_LOGIC_VECTOR(n-1 downto 0);
-    R:   OUT STD_LOGIC_VECTOR(n-1 downto 0)
+    R:   OUT STD_LOGIC_VECTOR(n-1 downto 0);
+	 yo:  OUT INTEGER
   );
 END logicShift;
 
 ARCHITECTURE LogicFunction OF logicShift IS
+SIGNAL lacie: INTEGER;
 BEGIN
      --sll
-R <= (STD_LOGIC_VECTOR(shift_left(UNSIGNED(A), to_integer(UNSIGNED(B))))) WHEN OP = "00" ELSE
+	  lacie <= to_integer(unsigned(B));
+R <= (STD_LOGIC_VECTOR(shift_left(unsigned(A), lacie))) WHEN OP = "00" ELSE
      --srl
-     (STD_LOGIC_VECTOR(shift_right(unsigned(A),to_integer(UNSIGNED(B))))) WHEN OP = "01" ELSE
+     (STD_LOGIC_VECTOR(shift_right(unsigned(A), lacie))) WHEN OP = "01" ELSE
      --sra
-     (STD_LOGIC_VECTOR(shift_left(signed(A), to_integer(UNSIGNED(B))))) WHEN OP = "10" ELSE;
+     (STD_LOGIC_VECTOR(shift_left(signed(A), lacie))) WHEN OP = "10";
 	  --ASSERT false REPORT "something went wrong, incorrect operation type";
      --error for 11?
 
