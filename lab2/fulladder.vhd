05f131e45014d5230fba1c73cbbb395218410fb2
Paige T <ptuttosi@sfu.ca>
2020-01-31 18:54:03 -0800

    start lab 2

---
+++ lab2/fulladder.vhd
@@ -0,0 +1,17 @@
+LIBRARY ieee ;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY fulladder IS
+  GENERIC(n: INTIGER:= 64)
+  PORT(
+    A,B, CIN: IN STD_LOGIC;;
+	 CIN: IN STD_LOGIC; 
+	 SUM,COUT: OUT STD_LOGIC
+  );
+ END fulladder;
+	 
+ARCHITECTURE LogicFunction OF fulladder IS
+  BEGIN
+   SUM <= (A XOR B) XOR CIN;
+	COUT <= (A AND B) OR (CIN AND A) OR (CIN AND B);
+END LogicFunction;
