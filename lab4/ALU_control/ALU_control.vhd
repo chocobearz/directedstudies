27d6876e21cc393866b868b15720269d801500e6
Paige T <ptuttosi@sfu.ca>
2020-03-29 17:20:33 -0700

    remove testing output

--- lab4/ALU_control/ALU_control.vhd
+++ lab4/ALU_control/ALU_control.vhd
@@ -17,17 +17,17 @@ BEGIN
   BEGIN
 	 IF (ALUOp = "00" OR ((ALUOp = "11" OR ALUOp = "10") AND Funct7 = "0000000" AND Funct3 = "000")) THEN
 	   Opcode <= "0000";
-	 ELSIF (ALUOp = "01" OR ((ALUOp = "11" OR ALUOp = "10") AND Funct7 = "0100000" AND Funct3 = "000")) THEN
+	 ELSIF (ALUOp = "01" OR (ALUOp = "10" AND Funct7 = "0100000" AND Funct3 = "000")) THEN
 	   Opcode <= "0010";
 	 ELSIF ((ALUOp = "11" OR ALUOp = "10") AND Funct7 = "0000000" AND Funct3 = "111") THEN
 	   Opcode <= "0111";
 	 ELSIF ((ALUOp = "11" OR ALUOp = "10") AND Funct7 = "0000000" AND Funct3 = "110") THEN
 	   Opcode <= "0110";
-	 ELSIF ((ALUOp = "11" OR ALUOp = "10") AND Funct7 = "0110011" AND Funct3 = "100") THEN
+	 ELSIF ((ALUOp = "11" OR ALUOp = "10") AND Funct7 = "0000000" AND Funct3 = "100") THEN
       Opcode <= "0100";
-	 ELSIF ((ALUOp = "11" OR ALUOp = "10") AND Funct7 = "0000000" AND Funct3 = "001") THEN
+	 ELSIF ((ALUOp = "10" OR ALUOp = "10") AND Funct7 = "0000000" AND Funct3 = "001") THEN
       Opcode <= "0001";
-	 ELSIF ((ALUOp = "11" OR ALUOp = "10") AND Funct7 = "0010011" AND Funct3 = "101") THEN
+	 ELSIF (((ALUOp = "11" AND Funct7 = "0010011") OR (ALUOp = "11" AND Funct7 = "0100000"))AND Funct3 = "101") THEN
 	   Opcode <= "0011";
 	 ELSIF ((ALUOp = "11" OR ALUOp = "10") AND Funct7 = "0000000" AND Funct3 = "101") THEN
 	   Opcode <= "0101";
