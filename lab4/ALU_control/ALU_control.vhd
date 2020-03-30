d7094f9424d5df1692a82ff46f6d2e9517471d4f
Paige T <ptuttosi@sfu.ca>
2020-03-30 10:33:08 -0700

    update ALUop conditions

--- lab4/ALU_control/ALU_control.vhd
+++ lab4/ALU_control/ALU_control.vhd
@@ -15,22 +15,39 @@ ARCHITECTURE logic_function OF ALU_control IS
 BEGIN
   PROCESS (ALUOp, Funct7, Funct3) IS
   BEGIN
-	 IF (ALUOp = "00" OR ((ALUOp = "11" OR ALUOp = "10") AND Funct7 = "0000000" AND Funct3 = "000")) THEN
+  --ADD
+   -- if store or load, or r-type with func7 = 0000000 and funct3 = 000 , or i-type and funct 3 = 000
+	 IF (ALUOp = "00" OR ( ((ALUOp = "10"AND Funct7 = "0000000") OR ALUOp = "11") AND Funct3 = "000") ) THEN
 	   Opcode <= "0000";
+	--SUB
+	-- if branch or r-type with funct7 = 0100000 and funct3 = 000
 	 ELSIF (ALUOp = "01" OR (ALUOp = "10" AND Funct7 = "0100000" AND Funct3 = "000")) THEN
 	   Opcode <= "0010";
-	 ELSIF ((ALUOp = "11" OR ALUOp = "10") AND Funct7 = "0000000" AND Funct3 = "111") THEN
+	-- AND
+	-- r or i type and funct3 = 111
+	 ELSIF ((ALUOp = "11" OR ALUOp = "10") AND Funct3 = "111") THEN
 	   Opcode <= "0111";
-	 ELSIF ((ALUOp = "11" OR ALUOp = "10") AND Funct7 = "0000000" AND Funct3 = "110") THEN
+	-- OR
+	-- r or i type and funct3 = 110
+	 ELSIF ((ALUOp = "11" OR ALUOp = "10") AND Funct3 = "110") THEN
 	   Opcode <= "0110";
-	 ELSIF ((ALUOp = "11" OR ALUOp = "10") AND Funct7 = "0000000" AND Funct3 = "100") THEN
+	-- XOR
+	-- r or i type and funct3 = 100
+	 ELSIF ((ALUOp = "11" OR ALUOp = "10") AND Funct3 = "100") THEN
       Opcode <= "0100";
-	 ELSIF ((ALUOp = "10" OR ALUOp = "10") AND Funct7 = "0000000" AND Funct3 = "001") THEN
+	-- SLL
+	-- r or i type and funct3 = 001
+	 ELSIF ((ALUOp = "10" OR ALUOp = "10") AND Funct3 = "001") THEN
       Opcode <= "0001";
+	-- SRA
+	-- r and funct7 = 0010011 or r and funct7 = 0100000, and funct3 =101
 	 ELSIF (((ALUOp = "11" AND Funct7 = "0010011") OR (ALUOp = "11" AND Funct7 = "0100000"))AND Funct3 = "101") THEN
 	   Opcode <= "0011";
+	-- SRL
+	-- r or i type and funct7 = 0000000 and funct3 = 101
 	 ELSIF ((ALUOp = "11" OR ALUOp = "10") AND Funct7 = "0000000" AND Funct3 = "101") THEN
 	   Opcode <= "0101";
+	-- anything else we have don't care, this is not an option
     ELSE
 	   Opcode <= "----";
     END IF;
